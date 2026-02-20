28e59a916b6e412ae6a1df1df6b4cbee24e11458
Hong <ttah.hongtran@gmail.com>
2026-02-20 18:34:00 +0700

    Add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 
@@ -6,6 +7,17 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
+    
+    # Check if the response status code is 400 (bad request for blank entries)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     
     # Convert response.text into a dictionary
     response_dict = json.loads(response.text)
